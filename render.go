6420bc9d35bc5c3c8196809d0e4894ccbe61cf2b
sergey-lipin <sergey.lipin@gmail.com>
2025-01-21 19:04:58 -0800

    Remove logging

--- render.go
+++ render.go
@@ -6,10 +6,6 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"log"
-	"os"
-	"runtime/debug"
-	"sync"
 
 	"golang.org/x/net/html"
 )
@@ -20,9 +16,6 @@ type RenderContext struct {
 	Uploads UploadContext
 	Assigns interface{}
 }
-
-var _counters = map[SocketID]int{}
-var _countersMu sync.Mutex
 
 // RenderSocket takes the engine and current socket and renders it to html.
 func RenderSocket(ctx context.Context, e Engine, s Socket) (*html.Node, error) {
@@ -51,46 +44,6 @@ func RenderSocket(ctx context.Context, e Engine, s Socket) (*html.Node, error) {
 			err := s.Send(EventPatch, patches)
 			if err != nil {
 				return nil, fmt.Errorf("send error: %w", err)
-			}
-
-			// TEMP: Save the current and proposed renders to files.
-			_countersMu.Lock()
-			counter, ok := _counters[s.ID()]
-			if !ok {
-				counter = 0
-			}
-			_counters[s.ID()] = counter + 1
-			_countersMu.Unlock()
-
-			log.Println(string(debug.Stack()))
-
-			fc, err := os.OpenFile(fmt.Sprintf("%s-%d-current.html", s.ID(), counter), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err == nil {
-				defer fc.Close()
-
-				var d bytes.Buffer
-				html.Render(&d, s.LatestRender())
-
-				fc.WriteString(d.String())
-			}
-
-			fp, err := os.OpenFile(fmt.Sprintf("%s-%d-proposed.html", s.ID(), counter), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err == nil {
-				defer fp.Close()
-
-				var d bytes.Buffer
-				html.Render(&d, render)
-
-				fp.WriteString(d.String())
-			}
-
-			fr, err := os.OpenFile(fmt.Sprintf("%s-%d-results.html", s.ID(), counter), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err == nil {
-				defer fr.Close()
-
-				for _, p := range patches {
-					fr.WriteString(p.String() + "\n")
-				}
 			}
 		}
 	} else {
